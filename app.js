e556985e0a824a4b9a66980252394d9d0497df19
elchudi <gustavogiudici@gmail.com>
2015-02-24 13:55:47 +0100

    added domains for error handling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // DEPENDENCIES
+var domain = require('domain').create();
+
 var bunyan = require('bunyan');
 var log = bunyan.createLogger
 ({
@@ -8,60 +10,63 @@ var log = bunyan.createLogger
     name: "atahualpa",
     streams: [
         {
-            type: 'rotating-file',
-            path: '/var/log/atahualpa.log',
-            //path: '/var/log/atahualpa.log',
-            //level: 'info'
+            path: '/var/log/atahualpa.log'
         }
     ]
 });
 
 //giving bunyan a chance to flush
-process.on('uncaughtException', function (err) {
+domain.on('error', function(err){
     // prevent infinite recursion
-    process.removeListener('uncaughtException', arguments.callee);
-
     // bonus: log the exception
     log.fatal(err);
 
     if (typeof(log.streams[0]) !== 'object') return;
 
+    console.log('err');
+    console.log(err);
     // throw the original exception once stream is closed
     log.streams[0].stream.on('close', function(streamErr, stream) {
-        throw err;
+        console.log('err');
+        console.log(err);
+        console.log(streamErr);
+        process.exit(1);
     });
 
     // close stream, flush buffer to disk
     log.streams[0].stream.end();
 });
 
+domain.run(function(){
 
-var constants = require('constants');
+    var constants = require('constants');
 
-if (!process.argv[2]) {
-    log.info('Must give chaski info');
-    throw new Error('insuficient parameters given');
-}
+    if (!process.argv[2]) {
+        log.fatal('Must give chaski info');
+        throw new Error('insuficientarametersgiven');
+    }
 
-var IP_CHASKI = process.argv[2];
+    var IP_CHASKI = process.argv[2];
 
-var chaskiNotifier = require('./worker/chaskiNotifier')
-({
-    "ipChaski": IP_CHASKI,
-    "verbose" : constants.LOG_LEVEL_DEBUG
-});
+    var chaskiNotifier = require('./worker/chaskiNotifier')
+    ({
+        "ipChaski": IP_CHASKI,
+        "verbose" : constants.LOG_LEVEL_DEBUG
+    });
 
-var chaskiAssigner = require('./worker/chaskiAssigner')
-({
-    "ipChaski": IP_CHASKI,
-    "verbose" : constants.LOG_LEVEL_DEBUG, 
-    "chaskiNotifier": chaskiNotifier
-});
+    var chaskiAssigner = require('./worker/chaskiAssigner')
+    ({
+        "ipChaski": IP_CHASKI,
+        "verbose" : constants.LOG_LEVEL_DEBUG, 
+        "chaskiNotifier": chaskiNotifier
+    });
 
-var messageForwarder = require('./worker/messageForwarder')
-();
+    var messageForwarder = require('./worker/messageForwarder')
+    ();
 
-var messageReceiver = require('./worker/messageReceiver')
-({
-    "messageForwarder": messageForwarder
+    var messageReceiver = require('./worker/messageReceiver')
+    ({
+        "messageForwarder": messageForwarder
+    });
+
 });
